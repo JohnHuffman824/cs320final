151af4874fd90c64afd9956569b5bb1d6b6020ca
Jack Huffman <jackhuffman424@gmail.com>
2024-02-22 14:27:50 -0600

    added files

---
+++ project.py
@@ -0,0 +1,7 @@
+# This is the code to be written
+
+# Could we just us
+
+
+# Try and start using keras as our base for the neural netowrk
+# May be able toa  start with logistic regression for a MVP
